5ccfec5661bd826e118157a3cdf1f8abe5b28d09
Joseph Flatley <Jflatley487@gmail.com>
2023-07-29 21:33:50 -0500

    centralizedConnect

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,22 @@
 const mongoose = require("mongoose"); // Import mongoose lib
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/rectify"
-); // Connect to MongoDB
 
-module.exports = mongoose.connection; // Export the mongoose connection
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/rectify", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+  });
+  console.log('MongoDB Connected');
+  return conn;
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+/*mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/rectify"
+); // Connect to MongoDB*/
+
+module.exports = connectDB; // Export the connection
