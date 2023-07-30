966ce3625d3c639ccad150c8007a5a33c8262a10
Joseph Flatley <Jflatley487@gmail.com>
2023-07-29 23:01:40 -0500

    heroku connect

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose"); // Import mongoose lib
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGO_URI || "mongodb://127.0.0.1:27017/rectify", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,useUnifiedTopology: true,}
       );
   console.log('MongoDB Connected');
